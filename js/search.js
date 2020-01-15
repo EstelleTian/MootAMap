193ca861307d698c4307657ffb481c5361697fe7
zxydev <zxydev@163.coom>
2020-01-15 11:22:08 +0800

    查询分类路线表单显隐设置

--- js/search.js
+++ js/search.js
@@ -1,8 +1,14 @@
+/**
+ * 创建地图
+ * */
 var map = new AMap.Map('container', {
-    zoom: 13,
+    zoom: 13, //级别
     resizeEnable: true,
 });
 
+/**
+ *
+ * */
 AMapUI.loadUI(['misc/PoiPicker'], function(PoiPicker) {
     var poiPicker = new PoiPicker({
         //city:'北京',
@@ -118,5 +124,36 @@ document.getElementById('lukaung-btn').onclick = function(e){
         e.target.innerHTML = "隐藏实时路况";
     }
 };
+
+$('#search .fa-dir').on('click',function () {
+    $('#amapBox').hide();
+    $('#dirBox').show();
+});
+$('#dirBox .dir_close').on('click',function () {
+    $('#dirBox').hide();
+});
+$('#search #searchipt').on('click',function () {
+    var $this = $(this);
+    var val = $this.val();
+    if(val == ""){
+        $('#amapBox').show();
+    }else {
+        $('#amapBox').hide();
+    }
+});
+$('#search #searchipt').on('keyup',function () {
+    var $this = $(this);
+    var val = $this.val();
+    if(val == ""){
+        $('#amapBox').show();
+    }else {
+        $('#amapBox').hide();
+    }
+});
+$('#container').on('click',function (e) {
+    if($("#amapBox").is(':visible')){
+        $('#amapBox').hide();
+    }
+});
 
 
